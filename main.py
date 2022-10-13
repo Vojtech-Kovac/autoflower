3bf09decbf51db58e70f866e81fea353844dec93
Vojtech-Kovac <96598424+Vojtech-Kovac@users.noreply.github.com>
2022-10-13 16:26:40 +0200

    Přidány poznámky atd

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+WaterTankNOWATER = 0
 Waterleveltank = 0
 Waterlevelkytka = 0
 OLED.init(128, 64)
@@ -17,26 +18,61 @@ for index in range(3):
     basic.pause(500)
 basic.clear_screen()
 music.play_tone(988, music.beat(BeatFraction.WHOLE))
-basic.pause(500)
+basic.pause(400)
 music.play_tone(988, music.beat(BeatFraction.WHOLE))
 
 def on_forever():
     global Waterlevelkytka, Waterleveltank
     Waterlevelkytka = Environment.read_soil_humidity(AnalogPin.P1)
     Waterleveltank = Environment.read_water_level(AnalogPin.P2)
-    OLED.write_string_new_line("Flower Level" + str(Waterlevelkytka))
-    OLED.write_string_new_line("Tank Level:" + str(Waterleveltank))
+    OLED.write_string_new_line("Flower Level" + ("" + str(Waterlevelkytka)))
+    OLED.write_string_new_line("Tank Level:" + ("" + str(Waterleveltank)))
     basic.pause(5000)
-    if Waterlevelkytka > 30:
-        OLED.write_string_new_line("Flower is dry")
-        OLED.write_string_new_line("Flower Level:" + str(Waterlevelkytka))
-        basic.pause(2000)
-        pins.digital_write_pin(DigitalPin.P3, 1)
-        if Waterlevelkytka == 70:
-            pins.digital_write_pin(DigitalPin.P3, 0)
-    if Waterleveltank > 30:
-        OLED.write_string_new_line("ERROR :" + "Water level in tank is too low")
-        OLED.write_string_new_line("I can't start a creep sequence")
-        OLED.write_string_new_line("")
-        OLED.write_string_new_line("FIX :" + "Refill the tank")
 basic.forever(on_forever)
+
+"""
+
+Když je "WaterTankNOWATER" "0" tak se spustí další command když je "Waterlevelkytka" ">30"
+
+tak se zobrazí text flower is dry a čerpadlo pustí vodu dokud vlhkost pudy nebude 70%
+
+"""
+
+def on_forever2():
+    if WaterTankNOWATER == 0:
+        if Waterlevelkytka > 30:
+            OLED.write_string_new_line("Flower is dry")
+            OLED.write_string_new_line("Flower Level:" + ("" + str(Waterlevelkytka)))
+            basic.pause(2000)
+            pins.digital_write_pin(DigitalPin.P3, 1)
+            if Waterlevelkytka == 70:
+                pins.digital_write_pin(DigitalPin.P3, 0)
+basic.forever(on_forever2)
+
+"""
+
+Takzavně 
+
+Pokud je v nádrží málo vody konkrétně méně než 30% tak nastaví "WaterTankNOWATER"
+
+na "1" tím se zakáže používaní čerpadla
+
+a napíše ERROR že nemá vodu
+
+Jinak nastaví "WaterTankNOWATER"
+
+"0" Tím pádem se muže používat čerpadlo
+
+"""
+
+def on_forever3():
+    global WaterTankNOWATER
+    if Waterleveltank > 30:
+        WaterTankNOWATER = 1
+        OLED.write_string_new_line("ERROR :" + "Malo vody v nadrzy")
+        OLED.write_string_new_line("Nemuzu zalit kytku")
+        OLED.write_string_new_line("")
+        OLED.write_string_new_line("FIX :" + "Dopln nadrz vodou")
+    else:
+        WaterTankNOWATER = 0
+basic.forever(on_forever3)
--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let WaterTankNOWATER = 0
 let Waterleveltank = 0
 let Waterlevelkytka = 0
 OLED.init(128, 64)
@@ -18,7 +19,7 @@ for (let index = 0; index < 3; index++) {
 }
 basic.clearScreen()
 music.playTone(988, music.beat(BeatFraction.Whole))
-basic.pause(500)
+basic.pause(400)
 music.playTone(988, music.beat(BeatFraction.Whole))
 basic.forever(function () {
     Waterlevelkytka = Environment.ReadSoilHumidity(AnalogPin.P1)
@@ -26,19 +27,46 @@ basic.forever(function () {
     OLED.writeStringNewLine("Flower Level" + ("" + Waterlevelkytka))
     OLED.writeStringNewLine("Tank Level:" + ("" + Waterleveltank))
     basic.pause(5000)
-    if (Waterlevelkytka > 30) {
-        OLED.writeStringNewLine("Flower is dry")
-        OLED.writeStringNewLine("Flower Level:" + ("" + Waterlevelkytka))
-        basic.pause(2000)
-        pins.digitalWritePin(DigitalPin.P3, 1)
-        if (Waterlevelkytka == 70) {
-            pins.digitalWritePin(DigitalPin.P3, 0)
+})
+/**
+ * Když je "WaterTankNOWATER" "0" tak se spustí další command když je "Waterlevelkytka" ">30"
+ * 
+ * tak se zobrazí text flower is dry a čerpadlo pustí vodu dokud vlhkost pudy nebude 70%
+ */
+basic.forever(function () {
+    if (WaterTankNOWATER == 0) {
+        if (Waterlevelkytka > 30) {
+            OLED.writeStringNewLine("Flower is dry")
+            OLED.writeStringNewLine("Flower Level:" + ("" + Waterlevelkytka))
+            basic.pause(2000)
+            pins.digitalWritePin(DigitalPin.P3, 1)
+            if (Waterlevelkytka == 70) {
+                pins.digitalWritePin(DigitalPin.P3, 0)
+            }
         }
     }
+})
+/**
+ * Takzavně 
+ * 
+ * Pokud je v nádrží málo vody konkrétně méně než 30% tak nastaví "WaterTankNOWATER"
+ * 
+ * na "1" tím se zakáže používaní čerpadla
+ * 
+ * a napíše ERROR že nemá vodu
+ * 
+ * Jinak nastaví "WaterTankNOWATER"
+ * 
+ * "0" Tím pádem se muže používat čerpadlo
+ */
+basic.forever(function () {
     if (Waterleveltank > 30) {
+        WaterTankNOWATER = 1
         OLED.writeStringNewLine("ERROR :" + "Malo vody v nadrzy")
         OLED.writeStringNewLine("Nemuzu zalit kytku")
         OLED.writeStringNewLine("")
         OLED.writeStringNewLine("FIX :" + "Dopln nadrz vodou")
+    } else {
+        WaterTankNOWATER = 0
     }
 })
