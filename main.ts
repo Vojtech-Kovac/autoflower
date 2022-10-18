a7aaa4b6602a066dd3ba44c06a06dc1dd78ae188
Vojtech-Kovac <96598424+Vojtech-Kovac@users.noreply.github.com>
2022-10-18 09:26:01 +0200

    Update

--- main.ts
+++ main.ts
@@ -1,14 +1,4 @@
 input.onButtonPressed(Button.A, function () {
-    STOP = 1
-    OLED.clear()
-    OLED.writeStringNewLine("STOPED")
-    basic.showIcon(IconNames.No)
-    music.playTone(988, music.beat(BeatFraction.Double))
-})
-// Po zmáčknutí A+B
-// 
-// Se všechno zastaví
-input.onButtonPressed(Button.B, function () {
     STOP = 0
     OLED.clear()
     OLED.writeStringNewLine("STARTED")
@@ -18,7 +8,16 @@ input.onButtonPressed(Button.B, function () {
     music.playTone(988, music.beat(BeatFraction.Whole))
     OLED.clear()
 })
-let WaterTankNOWATER = 0
+// Po zmáčknutí A+B
+// 
+// Se všechno zastaví
+input.onButtonPressed(Button.B, function () {
+    STOP = 1
+    OLED.clear()
+    OLED.writeStringNewLine("STOPED")
+    basic.showIcon(IconNames.No)
+    music.playTone(988, music.beat(BeatFraction.Double))
+})
 let Waterleveltank = 0
 let Waterlevelkytka = 0
 let STOP = 0
@@ -30,35 +29,52 @@ basic.showLeds(`
     # # # # #
     # # # # #
     `)
-for (let index = 0; index < 3; index++) {
-	
-}
-basic.clearScreen()
+OLED.drawLoading(25)
+basic.pause(200)
+OLED.drawLoading(50)
+basic.pause(200)
+OLED.drawLoading(75)
+basic.pause(100)
+OLED.drawLoading(100)
 music.playTone(988, music.beat(BeatFraction.Whole))
 basic.pause(400)
 music.playTone(988, music.beat(BeatFraction.Whole))
 basic.showIcon(IconNames.Yes)
 let stop_basic_info = 0
+OLED.clear()
 basic.forever(function () {
-    if (STOP == 0) {
-        Waterlevelkytka = Environment.ReadSoilHumidity(AnalogPin.P1)
-        Waterleveltank = Environment.ReadWaterLevel(AnalogPin.P2)
-        OLED.writeStringNewLine("Flower Level:" + Waterlevelkytka)
-        OLED.writeStringNewLine("Tank Level:" + Waterleveltank)
+    Waterlevelkytka = Environment.ReadSoilHumidity(AnalogPin.P1)
+    Waterleveltank = Environment.ReadWaterLevel(AnalogPin.P3)
+})
+basic.forever(function () {
+    if (stop_basic_info == 0) {
+        if (STOP == 0) {
+            OLED.writeStringNewLine("Flower Level:" + Waterlevelkytka)
+            OLED.writeStringNewLine("Tank Level:" + Waterleveltank)
+        }
+        basic.pause(5000)
+        OLED.clear()
     }
-    basic.pause(5000)
-    OLED.clear()
 })
 // Když je "WaterTankNOWATER" "0" tak se spustí další command když je "Waterlevelkytka" ">30"
 // 
 // tak se zobrazí text flower is dry a čerpadlo pustí vodu dokud vlhkost pudy nebude 70%
 basic.forever(function () {
     if (STOP == 0) {
+        let WaterTankNOWATER = 0
         if (WaterTankNOWATER == 0) {
             if (Waterlevelkytka < 30) {
-                OLED.writeStringNewLine("Flower is dry")
-                OLED.writeStringNewLine("Flower Level:" + Waterlevelkytka)
+                stop_basic_info = 1
+                OLED.drawLoading(Waterlevelkytka)
                 basic.pause(2000)
+                OLED.clear()
+                if (Waterlevelkytka < 70) {
+                    stop_basic_info = 1
+                    pins.digitalWritePin(DigitalPin.P2, 1)
+                } else {
+                    pins.digitalWritePin(DigitalPin.P2, 0)
+                    stop_basic_info = 0
+                }
             }
         }
     }
@@ -77,19 +93,7 @@ basic.forever(function () {
 // 
 // když
 basic.forever(function () {
-    if (STOP == 0) {
-        if (Waterleveltank > 30) {
-            WaterTankNOWATER = 1
-            stop_basic_info = 1
-            OLED.writeStringNewLine("ERROR :" + "Malo vody v nadrzy")
-            OLED.writeStringNewLine("ERROR :" + "Nemuzu zalit kytku")
-            OLED.writeStringNewLine("")
-            OLED.writeStringNewLine("FIX :" + "Dopln nadrz vodou")
-        } else {
-            WaterTankNOWATER = 0
-            stop_basic_info = 0
-        }
-    }
+	
 })
 basic.forever(function () {
 	
